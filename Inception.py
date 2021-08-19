32aed3b37ae5326c98e52bfce06e9b01148cbb18
Saurav Mishra <saurav.mishra@live.com>
2021-08-19 17:33:04 +0530

    update as per the paper implementation
    @ https://arxiv.org/pdf/1409.4842.pdf

--- Inception.py
+++ Inception.py
@@ -6,54 +6,56 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Gaus
 from tensorflow.keras.layers import Activation, Dropout, ZeroPadding3D, GlobalAveragePooling2D, GlobalMaxPooling2D
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import plot_model
 
 class InceptionFramework:
     
-    def __init__():
+    def __init__(self):
         
         pass
     
     
-    def vanilla_inception_module(in_layer, f1, f2, f3):
+    def Naive_Inception_Module(self, in_layer, f1x1, f3x3, f5x5):
         
         # 1x1 conv
-        conv1x1 = Conv2D(f1, (1,1), padding='same', activation=tf.nn.relu)(in_layer)
+        conv1x1 = Conv2D(f1x1, (1,1), padding='same', activation=tf.nn.relu)(in_layer)
         
         # 3x3 conv
-        conv3x3 = Conv2D(f2, (3,3), padding='same', activation=tf.nn.relu)(in_layer)
+        conv3x3 = Conv2D(f3x3, (3,3), padding='same', activation=tf.nn.relu)(in_layer)
         
         # 5x5 conv
-        conv5x5 = Conv2D(f3, (5,5), padding='same', activation=tf.nn.relu)(in_layer)
+        conv5x5 = Conv2D(f5x5, (5,5), padding='same', activation=tf.nn.relu)(in_layer)
         
         # 3x3 max pooling
-        pool = MaxPooling2D((3,3), strides=(1,1), padding=tf.nn.relu)(in_layer)
+        pool3x3 = MaxPooling2D((3,3), strides=(1,1), padding='same')(in_layer)
         
-        # concatenate the convolution layers, assumes filters/channels last
-        layer_out = concatenate([conv1x1, conv3x3, conv5x5, pool], axis = -1)
+        # concatenate the convolution and the pooling layers,
+        out_layer = concatenate([conv1x1, conv3x3, conv5x5, pool3x3], axis = -1)
         
-        return layer_out
+        return out_layer
     
-    def Create_Inception_Block(self, input_layer, f1, f2_in, f2_out, f3_in, f3_out, f4_out):
+    def Create_Inception_Block(self, in_layer, f1x1, f3x3_red, f3x3, f5x5_red, f5x5, fpool):
         
         # 1x1 conv
-        conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(input_layer)
+        conv1x1 = Conv2D(f1x1, (1,1), padding='same', activation=tf.nn.relu)(in_layer)
 
-        # 3x3 conv
-        conv3 = Conv2D(f2_in, (1,1), padding='same', activation='relu')(input_layer)
-        conv3 = Conv2D(f2_out, (3,3), padding='same', activation='relu')(conv3)
+        # 3x3 reduce and conv
+        conv3x3_red = Conv2D(f3x3_red, (1,1), padding='same', activation=tf.nn.relu)(in_layer)
+        conv3x3 = Conv2D(f3x3, (3,3), padding='same', activation=tf.nn.relu)(conv3x3_red)
 
-        # 5x5 conv
-        conv5 = Conv2D(f3_in, (1,1), padding='same', activation='relu')(input_layer)
-        conv5 = Conv2D(f3_out, (5,5), padding='same', activation='relu')(conv5)
+        # 5x5 reduce and conv
+        conv5x5_red = Conv2D(f5x5_red, (1,1), padding='same', activation=tf.nn.relu)(in_layer)
+        conv5x5 = Conv2D(f5x5, (5,5), padding='same', activation=tf.nn.relu)(conv5x5_red)
 
         # 3x3 max pooling
-        pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_layer)
-        pool = Conv2D(f4_out, (1,1), padding='same', activation='relu')(pool)
+        pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(in_layer)
+        pool = Conv2D(fpool, (1,1), padding='same', activation=tf.nn.relu)(pool)
 
-        # concatenate the output of the convolutional layers and pass on
-        # to the next layers
-        output_layer = concatenate([conv1, conv3, conv5, pool], axis=-1)
-        return output_layer
+        # concatenate the convolutional layers , poling layer and pass on
+        # to the next layers.
+        out_layer = concatenate([conv1x1, conv3x3, conv5x5, pool], axis=-1)
+        
+        return out_layer
     
     def Build_Inception_Network(self, INPUT_SHAPE=(135, 135, 3)):
         
